95256304550fbd385cda33c1adaebfc4fb752b70
Alan Russell <ajrussellaudio@gmail.com>
2018-10-24 23:10:27 +0100

    remove console.log

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-const LocationsList = ({ locations }) =>
-  console.log(locations) || (
-    <ul>
-      {locations.map(location => (
-        <li key={location.ChargeDeviceId}>{location.ChargeDeviceRef}</li>
-      ))}
-    </ul>
-  );
+const LocationsList = ({ locations }) => (
+  <ul>
+    {locations.map(location => (
+      <li key={location.ChargeDeviceId}>{location.ChargeDeviceRef}</li>
+    ))}
+  </ul>
+);
 
 export default LocationsList;
